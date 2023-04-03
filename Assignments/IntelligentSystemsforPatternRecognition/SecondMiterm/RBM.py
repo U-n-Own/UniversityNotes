e7916ef3dc50656a00a0b702f30201f767b125f4
U-n-Own <cenzogargano@gmail.com>
2023-04-04 00:41:41 +0200

    fixed some stuff in rbm implementation

--- Assignments/IntelligentSystemsforPatternRecognition/SecondMiterm/RBM.py
+++ Assignments/IntelligentSystemsforPatternRecognition/SecondMiterm/RBM.py
@@ -6,7 +6,14 @@ import random
 class RBM:
     
     def __init__(self, num_visible, num_hidden, epochs, learning_rate): 
-        # write document for init
+        """
+        Initializing the RBM parameters
+        :param num_visible: number of visible units
+        :param num_hidden: number of hidden units
+        :param epochs: number of epochs of training
+        :param learning_rate: learning rate of the model
+        """
+    
         self.num_visible = num_visible
         self.num_hidden = num_hidden
         self.learning_rate = learning_rate    
@@ -16,7 +23,7 @@ class RBM:
         
         # Initialize matrix W from  a normal distribution with mean 0 and stddev sq
         #self.weights = np.random.normal(mean = 0, stddev = np.sqrt(1.0/num_visible), size = (num_visible, num_hidden))
-        self.weights = np.random.uniform(low = -1, high = 1, size = (num_visible, num_hidden))
+        self.weights = np.random.uniform(low = -1, high = 1, size = (num_hidden, num_visible))
         
         # these are our b and c in the picture avaialble in the assignment document
         self.visible_bias = np.zeros(num_visible)
@@ -46,24 +53,19 @@ class RBM:
     
     
     def sample_hidden(self, visible, batch_size=10):
-        # Sample the hidden layer activations given the visible layer activations
-        # Direct sampling
+        # Sample the hidden layer activations given the visible layer : data
 
         # sigmoid(b+W^T*v)
-        prob_hidden = self.sigmoid(np.dot(visible, (self.weights)) + self.hidden_bias)
-        
-        # sample the hidden units given the probability
-        #hidden = np.random.binomial(1, prob_hidden)
+        prob_hidden = self.sigmoid(np.dot(visible, (self.weights).T) + self.hidden_bias)
         
         return prob_hidden
      
     def sample_visible(self, hidden):
         # Sample the visible layer activations given the hidden layer activations
-        # Direct sampling
         
         # calculate the probability of visible units given the hidden units
         
-        prob_visible = self.sigmoid(np.dot(hidden, np.transpose(self.weights)) + self.visible_bias)
+        prob_visible = self.sigmoid(np.dot(hidden, (self.weights)) + self.visible_bias)
         
         return prob_visible     
    
@@ -89,42 +91,46 @@ class RBM:
         reconstruction_error = np.sum((wake - dream)**2)
 
         self.weights += self.learning_rate * (wake - dream)
-        self.visible_bias += self.learning_rate * (np.sum(positive, axis=0) - np.sum(negative, axis=0))
-        self.hidden_bias += self.learning_rate * (np.sum(data, axis=0) - np.sum(recon_data, axis=0))
+        self.hidden_bias += self.learning_rate * (np.sum(positive, axis=0) - np.sum(negative, axis=0))
+        self.visible_bias += self.learning_rate * (np.sum(data, axis=0) - np.sum(recon_data, axis=0))
         
         return reconstruction_error
                 
     def contrastive_divergence_1(self, data, num_steps=1):
-        # Implement the CD-1 algorithm to train the RBM
+        """ 
+        Implement the CD-1 algorithm to train the RBM
+        """ 
         
-        # Positive divergence 
+        # Positive divergence : 
         positive_hi_prob = self.sample_hidden(visible=data)
             
         # transform in probabilities to fire the hidden units 1 or 0
         # put at zero the probabilities that are less than 0.5
         probs_hidden = np.array(positive_hi_prob)
         
+        wake_phase = np.dot(data.T, probs_hidden)         
+
         # extract an array of n_hidden from a binomial and put to 1 the probs corresponding if the prob is higher
         hidden_units_sample = np.random.binomial(1, probs_hidden, size=probs_hidden.shape)
                     
-        #for prob, sampled_hidden in zip(probs_hidden, hidden_units_sample):
         activation_mask = probs_hidden >= hidden_units_sample
         
         for i, activated in enumerate(activation_mask):
             probs_hidden[i][activated] = 1
             probs_hidden[i][~activated] = 0
         
-        #print("Positive hidden batch probabilities shape: ", probs_hidden)
-            
-        wake_phase = np.dot(data.T, probs_hidden)         
-        
         
         # Negative divergence
 
         recon_data_prob = self.sample_visible(hidden=probs_hidden)
 
-        #print("Reconstructed data shape: ", recon_data_prob.shape)
-        #print("Reconstructed data: ", recon_data_prob) 
+        reconstruction_sample = np.random.binomial(1, recon_data_prob, size=recon_data_prob.shape)
+        
+        activation_mask = recon_data_prob >= reconstruction_sample
+        
+        for i, activated in enumerate(activation_mask):
+            recon_data_prob[i][activated] = 1
+            recon_data_prob[i][~activated] = 0
         
         neg_hi_probs = self.sample_hidden(visible=recon_data_prob) 
 
