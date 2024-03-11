81833dea07b393055881e54c0260e1111cbfb3ad
U-n-Own <cenzogargano@gmail.com>
2024-03-11 11:52:47 +0100

    updated start of first assignment of CNS

---
+++ Assignments/ComputationalNeuroscience/izichievich_mode.py
@@ -0,0 +1,7 @@
+# Import libraries for simlation of Hodgkin-Huxley model and Izhikevich model
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+
+# Hodgkin-Huxley model
