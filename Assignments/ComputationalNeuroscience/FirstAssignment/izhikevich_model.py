bb5ccc6c2ff5d23e38499046aeaabfc783db877a
U-n-Own <cenzogargano@gmail.com>
2024-03-14 15:15:20 +0100

    izhikevich model added some simulation

--- Assignments/ComputationalNeuroscience/FirstAssignment/izhikevich_model.py
+++ Assignments/ComputationalNeuroscience/FirstAssignment/izhikevich_model.py
@@ -1,6 +1,8 @@
 
 import numpy as np
 from scipy.integrate import odeint
+# RungeKutta    
+
 
 # Class for neuron types
 class IzhikevichModel():
@@ -21,7 +23,7 @@ class IzhikevichModel():
                                 u <- c, w <- w + d  
     """
 
-    def __init__(self, a, b, c, d, I, u = -65) -> None:
+    def __init__(self, a, b, c, d, I, w, u = -65) -> None:
         
         self.a = a #0.02
         self.b = b #0.2
@@ -30,35 +32,52 @@ class IzhikevichModel():
         self.I = I #5
         
         self.u = u
-        self.w = 0
+        self.w = w
         
-        self.dt = 0.1
+        self.dt = 0.25
     
     def dudt(self, I, t):
+        """
+        dudt is the first equation of the Izhikevich model actually not multiplying here by dt.
+        """
         return 0.04*self.u**2 + 5*self.u + 140 - self.w + I
     
     def dwdt(self):
+        """
+        dwdt is the second equation of the Izhikevich model actually not multiplying here by dt.
+        """
         return self.a*(self.b*self.u - self.w)
     
     
     def simulate(self, I, T):
-        
+        """Simulate the Izhikevich model by using Leap-Frog method."""
+        threshold = 30
         #t = np.arange(0, T, 0.1)
         t = np.arange(0, min(T, len(I)*0.1), 0.1)
+        
         u = [] 
         w = []
+        spikes = []
         
         for i in range(len(t)):
+            
             self.u += self.dt*self.dudt(I[i], t = t[i])
             self.w += self.dt*self.dwdt()
             
-            if self.u >= 30:
+            if self.u >= threshold:
+                
                 self.u = self.c
                 self.w += self.d
-            
-            u.append(self.u)
+                
+                u.append(30)
+        
+            else:
+                u.append(self.u)
             w.append(self.w)
                 
-        return t, u, w
+        return t, u, w, spikes
+    
+        
+        
     
      
