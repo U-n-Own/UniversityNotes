cf941bf7eee1c844a4598dd14bfb1127c0375ab6
U-n-Own <cenzogargano@gmail.com>
2024-04-16 00:25:25 +0200

    added permutation update instead of simple stochastic and gif visualization of convergence

--- Assignments/ComputationalNeuroscience/SecondAssignment/HopfieldNetwork.py
+++ Assignments/ComputationalNeuroscience/SecondAssignment/HopfieldNetwork.py
@@ -41,10 +41,19 @@ class HopfieldNet():
         """
         return -0.5*np.dot(np.dot(pattern.T, self.weights), pattern)
     
-    def overlap(self, pattern):
-        """Compute the overlap function
+    def overlap(self, pattern, recall_pattern):
         """
-    def recall(self, pattern, steps=100, dimension=32, bias=0.7):
+        Compute the overlap function: 
+        
+        m = 1/N * sum_i s_i * x_i
+        """
+        
+        return (1/len(pattern))*np.dot(pattern, recall_pattern)
+    
+        
+      
+      
+    def recall(self, pattern, steps=100, dimension=32, bias=0.7, by_frame=False):
         """
         Recall the pattern from the network, given a pattern choose randomly a
         neuron and save the update then do it for each neuron at least once
@@ -52,26 +61,42 @@ class HopfieldNet():
         bias = bias
         energy = []
         new_pattern = pattern.copy()
+        overlap = []
+        
+        # If by_frame is True, save each state of the pattern during the recall phase to see the evolution using visualization trough video
+        # with frame equal to the number of steps
+        frames = []
         
         for step_num in range(steps):
+            
             previous_pattern = new_pattern.copy()
-            for _ in range(dimension):
-                # choose a random neuron not yet updated                    
-                i = np.random.randint(0, dimension)
+            update_order = np.random.permutation(dimension)
+            
+            for update in range(dimension):
+                # choose a random permutation of the neurons
                 
-                # Update the i-th neuron
-                pattern[i] = np.sign(np.dot(self.weights[i], pattern) + bias)
-                 
+                pattern[update_order[update]] = np.sign(np.dot(self.weights[update_order[update]], pattern) + bias)
+                
+                #pattern[i] = np.sign(np.dot(self.weights[i], pattern) + bias)
+                
                 new_pattern = pattern.copy()
                 
+                if by_frame:
+                    frames.append(new_pattern)
+            
+                # compute overlap
+                flattened_old = previous_pattern.flatten()
+                flattened_new = new_pattern.flatten()
+                overlap.append(self.overlap(flattened_old, flattened_new))
+        
                 # compute the energy of the pattern
                 energy.append(self.energy(pattern))
                 
         # if the pattern is equal to the one at previous step, stop
             if np.array_equal(previous_pattern, new_pattern):
                 print('Pattern converged after {} steps'.format(step_num))
-                return pattern, energy
+                return pattern, energy, overlap, frames
     
         print('Pattern did not converge after {} steps'.format(steps))
-            
-        return pattern, energy
+        
+        return pattern, energy, overlap, frames
