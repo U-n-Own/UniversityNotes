897fdfec3602915ac7477f104f5d6793297c4243
U-n-Own <cenzogargano@gmail.com>
2024-04-16 16:22:30 +0200

    some adjustments to the code of the hopfield

--- Assignments/ComputationalNeuroscience/SecondAssignment/HopfieldNetwork.py
+++ Assignments/ComputationalNeuroscience/SecondAssignment/HopfieldNetwork.py
@@ -27,10 +27,11 @@ class HopfieldNet():
         
         # project weights matrix in a bigger space
         #weights = np.zeros((2048,2048))
-         
+        
+        # debug shapes for outer product
         for pattern in self.patterns:
             weights += (1/dimension)*np.outer(pattern, pattern)
-        
+            
         np.fill_diagonal(weights, 0)
         
         return weights
@@ -50,10 +51,8 @@ class HopfieldNet():
         
         return (1/len(pattern))*np.dot(pattern, recall_pattern)
     
-        
       
-      
-    def recall(self, pattern, steps=100, dimension=32, bias=0.7, by_frame=False):
+    def recall(self, pattern, steps=100, dimension=32, bias=0.7, by_frame=False, compute_energy=False):
         """
         Recall the pattern from the network, given a pattern choose randomly a
         neuron and save the update then do it for each neuron at least once
@@ -71,26 +70,28 @@ class HopfieldNet():
             
             previous_pattern = new_pattern.copy()
             update_order = np.random.permutation(dimension)
-            
+            print('Step number: {}'.format(step_num)) 
             for update in range(dimension):
                 # choose a random permutation of the neurons
                 
                 pattern[update_order[update]] = np.sign(np.dot(self.weights[update_order[update]], pattern) + bias)
-                
+
                 #pattern[i] = np.sign(np.dot(self.weights[i], pattern) + bias)
                 
                 new_pattern = pattern.copy()
                 
                 if by_frame:
                     frames.append(new_pattern)
-            
+
                 # compute overlap
                 flattened_old = previous_pattern.flatten()
                 flattened_new = new_pattern.flatten()
-                overlap.append(self.overlap(flattened_old, flattened_new))
+                
+                if compute_energy:
+                    overlap.append(self.overlap(flattened_old, flattened_new))
         
-                # compute the energy of the pattern
-                energy.append(self.energy(pattern))
+                    # compute the energy of the pattern
+                    energy.append(self.energy(pattern))
                 
         # if the pattern is equal to the one at previous step, stop
             if np.array_equal(previous_pattern, new_pattern):
