a76ce52442b43f85267d856459915f1e80d81d76
U-n-Own <cenzogargano@gmail.com>
2025-03-13 11:54:52 +0100

    Some more changes to avx version

--- Assignments/ParallelDistributedSystems/Assignment1/assignment1/softmax_avx.cpp
+++ Assignments/ParallelDistributedSystems/Assignment1/assignment1/softmax_avx.cpp
@@ -122,72 +122,6 @@ void softmax_avx_op2(const float *input, float *output, size_t K) {
     }
 }
 
-// softmax implementation this support alignment and scalar handling of remaining elements
-void softmax_avx_optimized(float* input, float* output, size_t K) {
-    // Find the maximum to stabilize the computation of the exponential
-
-    const size_t VEC_SIZE = 8;
-
-    float max_val = -std::numeric_limits<float>::infinity();
-
-    __m256 max_val_v = _mm256_set1_ps(max_val);
-    __m256 input_v;
-
-    size_t i = 0;
-    for (; i < K - VEC_SIZE + 1; i += VEC_SIZE) {
-        input_v = _mm256_loadu_ps(input + i);
-        max_val_v = _mm256_max_ps(max_val_v, input_v);
-    }
-
-    float max_val_arr[VEC_SIZE];
-    _mm256_storeu_ps(max_val_arr, max_val_v);
-
-    for (size_t j = 0; j < VEC_SIZE; ++j) {
-        max_val = std::max(max_val, max_val_arr[j]);
-    }
-
-    // computes all exponentials with the shift of max_val and the total sum 
-    float sum = 0.0f;
-
-    __m256 sum_v = _mm256_setzero_ps();
-    __m256 exp_v;
-
-    for (i = 0; i < K - VEC_SIZE + 1; i += VEC_SIZE) {
-        input_v = _mm256_loadu_ps(input + i);
-        exp_v = exp256_ps(_mm256_sub_ps(input_v, _mm256_set1_ps(max_val)));
-        _mm256_storeu_ps(output + i, exp_v);
-        sum_v = _mm256_add_ps(sum_v, exp_v);
-    }
-
-    float sum_arr[VEC_SIZE];
-    _mm256_storeu_ps(sum_arr, sum_v);
-    
-    for (size_t j = 0; j < VEC_SIZE; ++j) {
-        sum += sum_arr[j];
-    }
-
-    // scalar handling of remaining elements
-    for (; i < K; ++i) {
-        output[i] = std::exp(input[i] - max_val);
-        sum += output[i];
-    }
-
-    // normalize by dividing for the total sum
-    __m256 sum_v2 = _mm256_set1_ps(sum);
-    __m256 output_v;
-
-    for (i = 0; i < K - VEC_SIZE + 1; i += VEC_SIZE) {
-        output_v = _mm256_loadu_ps(output + i);
-        output_v = _mm256_div_ps(output_v, sum_v2);
-        _mm256_storeu_ps(output + i, output_v);
-    }
-
-    // scalar handling of remaining elements
-    for (; i < K; ++i) {
-        output[i] /= sum;
-    }
-}
-
 
 void printResult(std::vector<float> &v, size_t K) {
 	for(size_t i=0; i<K; ++i) {
@@ -214,20 +148,12 @@ int main(int argc, char *argv[]) {
 	}
 	size_t K = std::stol(argv[1]);
 	bool print = (argc >= 3) ? std::string(argv[2]) == "true" : false;
-	bool optimized = (argc >= 4) ? std::string(argv[3]) == "true" : false;
 
 	std::vector<float> input = generate_random_input(K);
 	std::vector<float> output(K);
 
-
 	TIMERSTART(softime_avx);
-	if (optimized) {
-		printf("Optimized version avx\n");
-        softmax_avx_op2(input.data(), output.data(), K);
-	} else {
-		printf("Standard avx\n");
 		softmax_avx(input.data(), output.data(), K);
-	}
 	TIMERSTOP(softime_avx);
 	
 	// print the results on the standard output
